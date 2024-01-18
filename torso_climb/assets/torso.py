3c6a377314b78f095f7899316ebe10fcbf764db1
dylan <goesdylan@gmail.com>
2024-01-18 20:04:36 +0000

    torso prototypes

---
+++ torso_climb/assets/torso.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pybullet as p
+import os
+import math
+
+
+class Torso:
+    def __init__(self, client, pos):
+        self.client = client
+        f_name = os.path.join(os.path.dirname(__file__), 'humanoid.xml')
+        self.id = p.loadURDF(fileName=f_name,
+                             basePosition=pos,
+                             physicsClientId=client)
+
+    def get_ids(self):
+        return self.id, self.client
+
+    def apply_action(self, action):
+        pass
+
+    def get_observation(self):
+        pass
--- torso_climb/env/torso_climb_env.py
+++ torso_climb/env/torso_climb_env.py
@@ -8,27 +8,32 @@ import math
 import pybullet as p
 import pybullet_data
 
+from torso_climb.assets.torso import Torso
+
 
 class TorsoClimbEnv(gym.Env):
     metadata = {'render_modes': ['human'], 'render_fps': 60}
 
     def __init__(self, render_mode: Optional[str] = None):
-
         self.render_mode = render_mode
 
-        self.client = p.connect(p.GUI if self.render_mode == 'human' else p.DIRECT)
-        print('human' if self.render_mode == 'human' else 'non-human')
+        if self.render_mode == 'human':
+            self.client = p.connect(p.GUI)
+        else:
+            self.client = p.connect(p.DIRECT)
 
         # action space
         # observation space
 
+        self.action_space = gym.spaces.Discrete(1)
+        self.observation_space = gym.spaces.Box(low=-float('inf'), high=float('inf'), shape=(1,), dtype=np.float32)
 
         self.np_random, _ = gym.utils.seeding.np_random()
 
         # configure pybullet GUI
-        p.setAdditionalSearchPath(pybullet_data.getDataPath())
-        p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
-        p.resetDebugVisualizerCamera(cameraDistance=10, cameraYaw=90, cameraPitch=0, cameraTargetPosition=[0, 0, 5])
+        p.setAdditionalSearchPath(pybullet_data.getDataPath(), physicsClientId=self.client)
+        p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0, physicsClientId=self.client)
+        p.resetDebugVisualizerCamera(cameraDistance=10, cameraYaw=-90, cameraPitch=0, cameraTargetPosition=[0, 0, 5], physicsClientId=self.client)
 
     def _get_obs(self):
         return dict()
@@ -37,8 +42,7 @@ class TorsoClimbEnv(gym.Env):
         return dict()
 
     def step(self, action):
-
-        p.stepSimulation()
+        p.stepSimulation(physicsClientId=self.client)
 
         # Gather information about the env
         ob = self._get_obs()
@@ -51,7 +55,7 @@ class TorsoClimbEnv(gym.Env):
         truncated = False
 
         if self.render_mode == 'human':
-            sleep(1/240)
+            sleep(1 / 240)
 
         return ob, reward, terminated, truncated, info
 
@@ -62,10 +66,12 @@ class TorsoClimbEnv(gym.Env):
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
 
-        p.resetSimulation(self.client)
-        p.setGravity(0, 0, -10)
+        p.resetSimulation(physicsClientId=self.client)
+        p.setGravity(0, 0, -9.8, physicsClientId=self.client)
 
-        plane = p.loadURDF("plane.urdf")
+        flags = p.URDF_MAINTAIN_LINK_ORDER + p.URDF_USE_SELF_COLLISION + p.URDF_USE_SELF_COLLISION_EXCLUDE_ALL_PARENTS
+        plane = p.loadURDF("plane.urdf", physicsClientId=self.client)
+        torso = Torso(client=self.client, pos=[0, 0, 5])
 
         ob = self._get_obs()
         info = self._get_info()
@@ -76,4 +82,4 @@ class TorsoClimbEnv(gym.Env):
         pass
 
     def close(self):
-        p.disconnect(self.client)
+        p.disconnect(physicsClientId=self.client)
