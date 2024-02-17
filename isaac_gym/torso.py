4f7e96522853b45745a8bfb14ad123937d126ca2
dylanjoao-linux <goesdylan@gmail.com>
2024-02-17 02:47:24 +0000

    Incomplete Torso and Wall

--- isaac_gym/torso.py
+++ isaac_gym/torso.py
@@ -2,6 +2,7 @@ import os
 import math
 from isaacgym import gymapi
 from isaacgym import gymutil
+import numpy as np
 
 # Initialize gym
 gym = gymapi.acquire_gym()
@@ -42,11 +43,57 @@ if viewer is None:
     print("*** Failed to create viewer")
     quit()
 
+torso_pose = gymapi.Transform()
+torso_pose.p = gymapi.Vec3(0.0, 1.0, 0.0)
+torso_pose.r = gymapi.Quat(-0.707107, 0.0, 0.0, 0.707107)
+
+wall_pose = gymapi.Transform()
+wall_pose.p = gymapi.Vec3(1.0, 1.5, 0.0)
+wall_pose.r = gymapi.Quat(0.0, 0.0, 0.0, 1.0)
+
+### Asset handling
+asset_root = "./assets"
+asset_options = gymapi.AssetOptions()
+asset_options.fix_base_link = True
+
+torso_asset_file = "mjcf/nv_torso.xml"
+torso_asset = gym.load_mjcf(sim, asset_root, torso_asset_file, asset_options)
+
+wall_dims = gymapi.Vec3(0.0, 3.0, 2.0)
+wall_asset = gym.create_box(sim, wall_dims.x, wall_dims.y, wall_dims.z, asset_options)
+
+# Set up the environment grid
+num_envs = 1
+spacing = 1.5
+env_lower = gymapi.Vec3(-spacing, 0.0, -spacing)
+env_upper = gymapi.Vec3(spacing, 3.0, spacing)
+
+gym.subscribe_viewer_keyboard_event(viewer, gymapi.KEY_R, "reset")
+
+# cache some common handles for later use
+envs = []
 
 
+print("Creating %d environments" % num_envs)
+num_per_row = int(math.sqrt(num_envs))
 
+for i in range(num_envs):
+    # Create env
+    env = gym.create_env(sim, env_lower, env_upper, num_per_row)
+    envs.append(env)
+
+    torso_handle = gym.create_actor(env, torso_asset, torso_pose, "torso", i, 0)
+    wall_handle = gym.create_actor(env, wall_asset, wall_pose, "wall", i, 0)
+
+gym.viewer_camera_look_at(viewer, envs[0], gymapi.Vec3(-3, 3, -3), gymapi.Vec3(1.5, 1, 0))
+
+initial_state = np.copy(gym.get_sim_rigid_body_states(sim, gymapi.STATE_ALL))
 
 while not gym.query_viewer_has_closed(viewer):
+
+    for evt in gym.query_viewer_action_events(viewer):
+        if evt.action == "reset" and evt.value > 0:
+            gym.set_sim_rigid_body_states(sim, initial_state, gymapi.STATE_ALL)
 
     # Step the physics
     gym.simulate(sim)
